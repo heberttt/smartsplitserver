07fa3f09363a4e1687ea88b735347d1ef5287514
heberttt <hebertp054@gmail.com>
2025-06-10 23:41:44 +0800

    init receiptparserservice

---
+++ receiptparserservice/src/main/java/com/smartsplit/receiptparserservice/ReceiptparserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.smartsplit.receiptparserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptparserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptparserserviceApplication.class, args);
+	}
+
+}
