9b8c5d9e3b97bb1499c8fc786f7113820628864d
heberttt <hebertp054@gmail.com>
2025-07-20 21:53:27 +0800

    removed accountservice_deprecated and authservice

--- accountservice_deprecated/src/main/java/com/smartsplit/accountservice/Exceptions/EmailNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.smartsplit.accountservice.Exceptions;
-
-public class EmailNotFoundException extends Exception{
-    public EmailNotFoundException(String message){
-        super(message);
-    }
-}
